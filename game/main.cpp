82dc26214d587ec2687896ecd52b06d3f4bd3a4e
Joeblesson <johanblesson@gmail.com>
2022-10-10 13:12:51 +0200

    wipe
    didn't work, trying again

--- game/main.cpp
+++ game/main.cpp
@@ -1,24 +1,2 @@
 #include <iostream>
 
-#include "gamesrc/game.h"
-
-int main() {
-    //Init Game engine
-    Game game;
-
-    //Game
-    while(game.running()) {
-        
-
-        //Update
-        game.update();
-
-
-        //Render
-        game.render();
-
-    }
-
-    return 0;
-
-}

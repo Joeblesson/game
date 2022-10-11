96d81e32786a5f479c6a57eecf8ea8cbf127b534
Joeblesson <johanblesson@gmail.com>
2022-10-11 20:46:00 +0200

    experiments with sf::Window
    we do a little window renaming

---
+++ game/include.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Network.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
--- game/main.cpp
+++ game/main.cpp
@@ -1,2 +1,41 @@
-#include <iostream>
+#include "include.h"
 
+using std::string;
+
+int main() {
+    int fps;
+    string title;
+    char choice;
+    sf::Window window(sf::VideoMode(800, 600), "cool window", sf::Style::Close);
+    
+    while(window.isOpen()) {
+        sf::Event event;
+        std::cout << "do you want to change the title (Y/N)? ";
+        std::cin >> choice;
+        switch (choice)
+        {
+        case 'Y':
+            std::cout << "Okay then: ";
+            std::cin >> title;
+            window.setTitle(title);
+            _sleep(5000);
+            window.close();
+            break;
+        case 'N':
+            std::cout << "Okay then...";
+            _sleep(1000);
+            window.close();
+            break;
+        default:
+            std::cout << "Wrong answer.\n";
+            _sleep(1000);
+            window.close();
+        }
+        while(window.pollEvent(event)) {
+            if(event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+    }
+    return 0;
+}
